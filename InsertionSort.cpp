fec761a53a664196487a7d578d61fbb3f640e0ae
kori756 <khorirachmatr@gmail.com>
2026-03-12 17:56:52 +0700

    membuat prosedur input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,3 +4,26 @@ using namespace std;
 int arr[20];    //membuat array dengan panjang data 20
 int n;          //membuat variable inputan n
 
+void input (){ //procedure input
+    while (true){
+        cout << "Masukan jumlah data pada array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray tidak boleh lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "=============================================" ;
+    cout << "===========Masukan Element Array=============" ;
+    cout << "=============================================" ;
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
+
